3dbb6ce976f0ef359e250cb9947dddef463d477d
Hongjinnyjinny <ghdtjddjr50@naver.com>
2022-08-16 16:23:48 +0900

    포스트 목록 페이지에 카테고리 기능 추가하기

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 import os
 
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
 
 class Post(models.Model):
     title = models.CharField(max_length=50)
@@ -16,6 +26,8 @@ class Post(models.Model):
 
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
 
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
+
     # author : 추후 작성 예정
 
     def __str__(self):
@@ -24,10 +36,10 @@ class Post(models.Model):
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
 
-def get_file_name(Self):
+def get_file_name(self):
     return os.path.basename(self.file_upload.name)
 
-def get_file_ext(Self):
+def get_file_ext(self):
     return self.get_file_name().split('.')[-1]
 
 
